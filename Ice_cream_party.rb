d44f18504b7f6cc2632ab94cf2310d1f9e7e5598
Hsing-Hui Hsu <hhhsu1@gmail.com>
2013-11-04 14:06:04 -0800

    Ice cream party

---
+++ Ice_cream_party.rb
@@ -0,0 +1,66 @@
+### ICE CREAM PARTY PLANNER
+
+hash = {}
+
+def add_invitee(hash)
+	puts "What is your name?"
+	name = gets.chomp
+	puts "What is your favorite ice cream flavor?"
+	flavor = gets.chomp
+	hash[name] = flavor
+	puts hash
+end
+
+add_invitee(hash)
+
+puts "Would you like to invite more people?"
+response = gets.chomp
+
+while response[0].downcase == "y"
+	add_invitee(hash)	
+  puts "Would you like to add more people?"
+  response = gets.chomp
+end
+
+#Report of guests listed alphabetically
+def report_by_name(hash)
+	names = hash.keys.sort
+	names.each do |name|
+		puts name + "'s favorite flavor is " + hash[name]
+	end
+end
+
+#Report of all guests who like each flavor
+def report_by_flavors(hash)
+	flavors = {}
+	flavor_array = []	
+	hash.each do |name, flavor|
+		if flavor_array.include? (flavor)
+			flavors[flavor].push(name)
+		#checks if hash already has given flavor as a key
+		#creates a list of names by retrieving key from original hash
+		else
+			flavors[flavor] = [name]
+			flavor_array.push flavor
+		end
+	end
+	puts flavors
+end
+
+x = true
+while x
+	puts "Would you like a report by name or by flavors? (input: name or flavor)"
+	report = gets.chomp
+	if report == "name"
+		report_by_name(hash)
+		x = false
+	elsif report == "flavor"
+		report_by_flavors(hash)
+		x = false
+	else
+		puts "Please enter name or flavor!"	
+		x = true
+	end
+
+end
+
